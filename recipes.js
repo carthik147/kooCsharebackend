a99e43d441ae228fb17bdcf4d1da1bba5b1a6b9a
carthik147 <vaddekarthikreddy2@gmail.com>
2025-03-09 16:13:52 +0530

    Add files via upload

---
+++ recipes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipesModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./user.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await RecipesModel.find({});
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
